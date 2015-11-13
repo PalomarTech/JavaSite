45fae52ef274a0bd9429e795d533e3f53ae2b084
csit <csit>
2015-11-12 17:01:08 -0800

    Added basic style to homepage and started implementing login functionality

--- ptcmb/home/views.py
+++ ptcmb/home/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.contrib.auth import authenticate, login
+from django.template import RequestContext
+
+from .forms import LoginForm
+
+from members.models import MemberData
+from django.contrib.auth.models import User
 
 def index(request):
-    return render(request, 'home/index.html')
+    form = LoginForm()
+    context = {'form': form}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/join/')
+    else:
+        return render(request, 'home/index.html', context)
 
 # Create your views here.
