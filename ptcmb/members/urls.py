c958d34cd4875b137b0c0b1d8929eb9ae5f594db
csit <csit>
2016-03-03 15:49:44 -0800

    Added basic settings page. Not customized.

--- ptcmb/members/urls.py
+++ ptcmb/members/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<name>[a-zA-Z0-9]+)/$', views.memberpage, name='memberpage'),
+    url(r'^(?P<name>[a-zA-Z0-9]+)/settings/$', views.settingspage, name='settingspage')
 ]
--- ptcmb/members/views.py
+++ ptcmb/members/views.py
@@ -10,3 +10,8 @@ def memberpage(request, name):
     member_object = MemberData.objects.get(name = name)
     context = {'Object': member_object}
     return render(request, 'members/memberpages/index.html', context)
+
+def settingspage(request,name):
+    member_object = MemberData.objects.get(name = name)
+    context = {'Object': member_object}
+    return render(request, 'members/settingspage/index.html', context)
